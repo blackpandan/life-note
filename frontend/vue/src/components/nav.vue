83ddbffe789f2a144158d7594134d06b48c22a04
blackpandan <faithgstc54@gmail.com>
2020-12-07 06:34:45 -0500

    clean code edit on nav

--- frontend/vue/src/components/nav.vue
+++ frontend/vue/src/components/nav.vue
@@ -1,103 +1,133 @@
 <template>
-<nav>
-  <v-app-bar app dark color="purple accent-3" elevate-on-scroll>
-     <v-app-bar-nav-icon @click="drawer = !drawer" class="pa-0"></v-app-bar-nav-icon>
-     <v-toolbar-title class="title font-weight-regular"><span class="life">Life</span><span class="life grey--text text--lighten-3 font-weight-normal">Book</span></v-toolbar-title>
-     <v-spacer></v-spacer>
-     <v-toolbar-items class="d-none d-sm-flex">
-       <v-btn dark color="purple accent-3" depressed to="/">
-       <v-icon left>home</v-icon>
-       <span class="font-weight-regular life-l">home</span>
-       </v-btn>
-       <v-btn dark color="purple accent-3" depressed to="/login">
-         <v-icon left>person</v-icon>
-         <span class="font-weight-regular life-l">login</span>
-       </v-btn>
-       
-     </v-toolbar-items>
-     <v-toolbar-items class="d-flex d-sm-none"> 
-        <v-btn icon to="/">
-         <v-icon>home</v-icon>
-       </v-btn>
-       <v-btn icon to="/login">
-         <v-icon>person</v-icon>
-       </v-btn>
+  <nav>
+    <!-- For The App Bar -->
+    <v-app-bar app dark color="purple accent-3" elevate-on-scroll>
+      <v-app-bar-nav-icon @click="drawer = !drawer" class="pa-0"></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="title font-weight-regular">
+        <span class="siteName">Life</span>
+        <span class="siteName grey--text text--lighten-3 font-weight-normal">Book</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- Navigation Links -->
+      <!-- For Tablets and Other Bigger Screens-->
+      <v-toolbar-items class="d-none d-sm-flex">
+
+        <v-btn dark color="purple accent-3" depressed to="/">
+          <v-icon left>home</v-icon>
+          <span class="font-weight-regular homeText">home</span>
+        </v-btn>
+        
+        <v-btn dark color="purple accent-3" depressed to="/login">
+          <v-icon left>person</v-icon>
+          <span class="font-weight-regular loginText">login</span>
+        </v-btn>
       
-     </v-toolbar-items>
-   </v-app-bar>
-   <v-navigation-drawer v-model="drawer" app temporary color="grey lighten-4">
-     <v-container fluid>
-       <v-avatar size="96"><v-img src="../assets/head.png" alt="hey"></v-img></v-avatar>
-       <v-card flat class="grey lighten-4">
-         <v-card-title class="ma-0 pa-2 ">anonymous</v-card-title>
-         <v-card-subtitle class="pa-0 pa-2">login@gmail.com</v-card-subtitle>
-       </v-card>
-       <v-divider color="font-size-5 purple accent-3"></v-divider>
-     </v-container>
-       <v-list rounded>
-           <v-list-item-group>
-             <v-list-item to="/">
-               <v-list-item-icon>
-                 <v-icon>home</v-icon>
-               </v-list-item-icon>
-               <v-list-item-content>
-                 <v-list-item-title>
-                 Home
-                 </v-list-item-title>
-               </v-list-item-content>
-             </v-list-item>
+      </v-toolbar-items>
+      
+      <!-- For Mobile Phones And Screen Smaller Than 600px -->
+      <v-toolbar-items class="d-flex d-sm-none">
+        
+        <v-btn icon to="/">
+          <v-icon>home</v-icon>
+        </v-btn>
+        <v-btn icon to="/login">
+          <v-icon>person</v-icon>
+        </v-btn>
+      
+      </v-toolbar-items>
+    
+    </v-app-bar>
 
-             <v-list-item to="/tasks">
-               <v-list-item-icon>
-                 <v-icon>event</v-icon>
-               </v-list-item-icon>
-               <v-list-item-content>
-                 <v-list-item-title>Tasks</v-list-item-title>
-               </v-list-item-content>
-             </v-list-item>
+    <!-- For The Navigation Drawer -->
+    <v-navigation-drawer v-model="drawer" app temporary color="grey lighten-4">
+      
+      <!-- For The Avatar and User Details (That Are Displayed) -->
+      <v-container fluid>
+        
+        <v-avatar size="96">
+          <v-img src="../assets/head.png" alt="hey"></v-img>
+        </v-avatar>
+        
+        <v-card flat class="grey lighten-4">
+          <v-card-title class="ma-0 pa-2">anonymous</v-card-title>
+          <v-card-subtitle class="pa-0 pa-2">login@gmail.com</v-card-subtitle>
+        </v-card>
+        <v-divider color="font-size-5 purple accent-3"></v-divider>
+      
+      </v-container>
+      
+      <!-- For Navigation Links In The Drawer-->
+      <v-list rounded>
+        <v-list-item-group>
+          
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon>home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
 
-             <v-list-item to="/projects">
-               <v-list-item-icon>
-                 <v-icon>assignment</v-icon>
-               </v-list-item-icon>
-               <v-list-item-content>
-                 <v-list-item-title>
-                   Projects
-                 </v-list-item-title>
-               </v-list-item-content>
-             </v-list-item>
+          <v-list-item to="/tasks">
+            <v-list-item-icon>
+              <v-icon>event</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Tasks</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
 
-             <v-list-item to="/login">
-               <v-list-item-icon>
-                 <v-icon>login</v-icon>
-               </v-list-item-icon>
-               <v-list-item-content>
-                 <v-list-item-title>
-                   login
-                 </v-list-item-title>
-               </v-list-item-content>
-             </v-list-item>
-           </v-list-item-group>
-       </v-list>
-   </v-navigation-drawer>
-</nav>
+          <v-list-item to="/projects">
+            <v-list-item-icon>
+              <v-icon>assignment</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Projects</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item to="/login">
+            <v-list-item-icon>
+              <v-icon>login</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>login</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
 export default {
-data(){
-    return{
-        drawer: true
-    }
-}
-}
+  data() {
+    return {
+      // This controls the drawer visibility
+      drawer: true,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.life{
+/* variable declarations for fonts */
+$loginAndHome: 0.95em;
+
+/* For Text */
+.siteName {
   font-size: 1em;
-  &-l{
-    font-size: 0.95em;
-  }
 }
+.loginText{
+    font-size: $loginAndHome;
+  }
+.homeText{
+    font-size: $loginAndHome;
+  }
 </style>
--- frontend/vue/src/views/Home.vue
+++ frontend/vue/src/views/Home.vue
@@ -1,170 +1,179 @@
 <template>
   <div>
     <h1 class="subtitle-1 grey--text text--darken-1">Welcome!</h1>
-    <div class="">
+    <div class>
       <v-card flat small class="pa-0 px-6 py-2 grey lighten-4 border-radius-lg">
-          <v-row class="pa-0">
-            <v-card-title class="cus-e pa-0 font-weight-regular">Add Events</v-card-title>
-            <v-spacer></v-spacer>
+        <v-row class="pa-0">
+          <v-card-title class="cus-e pa-0 font-weight-regular">Add Events</v-card-title>
+          <v-spacer></v-spacer>
 
-            <v-dialog v-model="dialog" width="59vw">
+          <v-dialog v-model="dialog" width="59vw">
             <template v-slot:activator="{on, attrs}">
-            <v-card-actions class="pa-0">
-            
-            <v-btn fab x-small dark depressed color="#24F900" class="pa-0" v-bind="attrs" v-on="on">
-              <v-icon class="white--text" color="white">add</v-icon>
-            </v-btn>
-            </v-card-actions>
+              <v-card-actions class="pa-0">
+                <v-btn
+                  fab
+                  x-small
+                  dark
+                  depressed
+                  color="#24F900"
+                  class="pa-0"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-icon class="white--text" color="white">add</v-icon>
+                </v-btn>
+              </v-card-actions>
             </template>
 
             <v-card>
               <v-form>
                 <v-container fluid>
                   <v-select :items="items" label="type" dense solo prepend-icon="event" flat></v-select>
-                <v-text-field label="title"></v-text-field>
-                <v-textarea rows="3" label="description"></v-textarea>
-                  <v-btn small dark class="purple accent-3"><span>submit</span></v-btn>
+                  <v-text-field label="title"></v-text-field>
+                  <v-textarea rows="3" label="description"></v-textarea>
+                  <v-btn small dark class="purple accent-3">
+                    <span>submit</span>
+                  </v-btn>
                 </v-container>
               </v-form>
             </v-card>
-
-            </v-dialog>
-          </v-row>
+          </v-dialog>
+        </v-row>
       </v-card>
-      <br>
+      <br />
       <v-divider></v-divider>
-      <br>
-      <v-expansion-panels focusable >
-        <v-expansion-panel >
+      <br />
+      <v-expansion-panels focusable>
+        <v-expansion-panel>
           <v-expansion-panel-header class="cus-h text-capitalize font-weight-light">
             go to the doctor
-             <template v-slot:actions>
-            <v-icon color="purple accent-3">
-              $expand
-            </v-icon>
-             </template>
-            </v-expansion-panel-header>
+            <template v-slot:actions>
+              <v-icon color="purple accent-3">$expand</v-icon>
+            </template>
+          </v-expansion-panel-header>
           <v-divider></v-divider>
-        <v-expansion-panel-content>
-          <v-column>
-           <br>
-          <p class="cus-p">i need to go to the doctor for my eye checkup</p>
-          <br>
-          <v-row>
-            <v-btn small dark color="#24F900">
-              <span class="cus-b">done</span>
-            </v-btn>
-         <v-spacer></v-spacer>
-         <v-btn small dark color="#F9D500">
-              <span class="cus-b">pause</span>
-            </v-btn>
-          <v-btn small class="error ml-3">
-            <span class="cus-b">delete</span>
-          </v-btn>
-           </v-row>
-          </v-column>
-        </v-expansion-panel-content>
+          <v-expansion-panel-content>
+            <v-column>
+              <br />
+              <p class="cus-p">i need to go to the doctor for my eye checkup</p>
+              <br />
+              <v-row>
+                <v-btn small dark color="#24F900">
+                  <span class="cus-b">done</span>
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn small dark color="#F9D500">
+                  <span class="cus-b">pause</span>
+                </v-btn>
+                <v-btn small class="error ml-3">
+                  <span class="cus-b">delete</span>
+                </v-btn>
+              </v-row>
+            </v-column>
+          </v-expansion-panel-content>
         </v-expansion-panel>
         <v-expansion-panel class="pause">
-          <v-expansion-panel-header class="cus-h text-capitalize font-weight-regular" disable-icon-rotate>
-            <span class="pause-text" >go to the doctor</span>
+          <v-expansion-panel-header
+            class="cus-h text-capitalize font-weight-regular"
+            disable-icon-rotate
+          >
+            <span class="pause-text">go to the doctor</span>
             <!-- <v-spacer></v-spacer> -->
             <!-- <v-icon class="pause-text">pause</v-icon> -->
-             <template v-slot:actions>
-            <v-icon color="#F9D500">pause</v-icon>
-          </template>
-            </v-expansion-panel-header>
+            <template v-slot:actions>
+              <v-icon color="#F9D500">pause</v-icon>
+            </template>
+          </v-expansion-panel-header>
           <v-divider></v-divider>
-        <v-expansion-panel-content>
-          <v-column>
-           <br>
-          <p class="cus-p">i need to go to the doctor for my eye checkup</p>
-          <br>
-          <v-row>
-            <v-btn small dark color="#F9D500">
-              <span class="cus-b">cotinue</span>
-            </v-btn>
-         <v-spacer></v-spacer>
-          <v-btn small class="error">
-            <span class="cus-b">delete</span>
-          </v-btn>
-           </v-row>
-          </v-column>
-        </v-expansion-panel-content>
-       
+          <v-expansion-panel-content>
+            <v-column>
+              <br />
+              <p class="cus-p">i need to go to the doctor for my eye checkup</p>
+              <br />
+              <v-row>
+                <v-btn small dark color="#F9D500">
+                  <span class="cus-b">cotinue</span>
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn small class="error">
+                  <span class="cus-b">delete</span>
+                </v-btn>
+              </v-row>
+            </v-column>
+          </v-expansion-panel-content>
         </v-expansion-panel>
         <v-expansion-panel class="complete">
-          <v-expansion-panel-header class="cus-h text-capitalize font-weight-regular" disable-icon-rotate>
-            <span class="complete-text" >go to the doctor</span>
-             <template v-slot:actions>
-            <v-icon color="#24F900">check</v-icon>
-          </template>
-            </v-expansion-panel-header>
+          <v-expansion-panel-header
+            class="cus-h text-capitalize font-weight-regular"
+            disable-icon-rotate
+          >
+            <span class="complete-text">go to the doctor</span>
+            <template v-slot:actions>
+              <v-icon color="#24F900">check</v-icon>
+            </template>
+          </v-expansion-panel-header>
           <v-divider></v-divider>
-        <v-expansion-panel-content>
-          <v-column>
-           <br>
-          <p class="cus-p">i need to go to the doctor for my eye checkup</p>
-          <br>
-          <v-row>
-            <v-btn small dark color="#24F900" depressed>
-              <span class="cus-b">complete</span>
-            </v-btn>
-         <v-spacer></v-spacer>
-          <v-btn small class="error">
-            <span class="cus-b">delete</span>
-          </v-btn>
-           </v-row>
-          </v-column>
-        </v-expansion-panel-content>
-       
+          <v-expansion-panel-content>
+            <v-column>
+              <br />
+              <p class="cus-p">i need to go to the doctor for my eye checkup</p>
+              <br />
+              <v-row>
+                <v-btn small dark color="#24F900" depressed>
+                  <span class="cus-b">complete</span>
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn small class="error">
+                  <span class="cus-b">delete</span>
+                </v-btn>
+              </v-row>
+            </v-column>
+          </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
-    
     </div>
   </div>
 </template>
 
 <script>
 export default {
-data(){
-  return{
-    dialog : false,
-    items : ["project", "event", "todo"]
-  }
-}
-}
+  data() {
+    return {
+      dialog: false,
+      items: ["project", "event", "todo"],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-$green: #24F900;
-.cus{
-  &-h{
+$green: #24f900;
+.cus {
+  &-h {
     font-size: 1.1em;
   }
-  &-p{
+  &-p {
     font-size: 1em;
   }
-  &-b{
+  &-b {
     font-size: 0.8em;
   }
-  &-e{
+  &-e {
     font-size: 1em;
   }
 }
-.pause{
-  border-left:0.2em solid #F9D500;
-  &-text{
-    color: #F9D500;
+.pause {
+  border-left: 0.2em solid #f9d500;
+  &-text {
+    color: #f9d500;
   }
 }
 
-.complete{
-    border-left: 0.2em solid $green;
-    // background-color: black;
-    &-text{
-      color: $green ;
-    }
+.complete {
+  border-left: 0.2em solid $green;
+  // background-color: black;
+  &-text {
+    color: $green;
   }
-
+}
 </style>
