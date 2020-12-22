a6dade68f96b36377150e5c5f3402de4bb922b16
blackpandan <faithgstc54@gmail.com>
2020-12-21 23:38:41 -0500

    added functionality for adding project and set up modify_projects function

--- backend/api/core/views.py
+++ backend/api/core/views.py
@@ -21,11 +21,35 @@ def get_all_todos(request):
     except:
         return Response(request, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def get_all_projects(request):
-    data = Project.objects.all()
-    serial = ProjectSerializer(data, many=True)
-    return Response(serial.data)
+# this is the configuration for the projects section request handling
+@api_view(['GET', 'POST'])
+def get_all_projects(request, id):
+    if (request.method == "GET"):
+        data = Project.objects.all()
+        serial = ProjectSerializer(data, many=True)
+        return Response(serial.data)
+    elif(request.method == 'POST'):
+        try:
+            name = request.data['title']
+            details = request.data['details']
+            new = Project(title=name, body=details)
+            # serial = ProjectSerializer(new)
+            new.save()
+            return Response("sucessfully added", status=status.HTTP_201_CREATED)
+        except KeyError as e:
+            return Response(f"required field is missing", status=status.HTTP_428_PRECONDITION_REQUIRED)
+
+
+@api_view(["PUT"])
+def modify_projects(request, id):
+    if (request.method == "PUT"):
+        try:
+            name = request.data['title']
+            details = request.data['details']
+            time = request.data['time']
+            return Response("tested")
+        except KeyError as e:
+            return Response("please provide required field", status=status.HTTP_428_PRECONDITION_REQUIRED)
 
 @api_view(['POST','PUT'])
 def delete_todo(request, pk):
