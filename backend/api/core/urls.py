c72cbe36afc3f8f94bce1a30416185f5aacefa77
blackpandan <faithgstc54@gmail.com>
2020-12-21 23:48:36 -0500

    changed modify route kwarg pk to id

--- backend/api/core/urls.py
+++ backend/api/core/urls.py
@@ -3,7 +3,7 @@ from .views import get_all_todos, get_all_projects, delete_todo, modify_projects
 
 urlpatterns = [
     path("", get_all_projects),
-    path("projects/modify/<int:pk>", modify_projects),
+    path("projects/modify/<int:id>", modify_projects),
     path("todos-all", get_all_todos),
     path("delete/todo/<int:pk>", delete_todo)
 ]
