e306df1e4b074ee6c655daf68ba0847ab0321649
blackpandan <faithgstc54@gmail.com>
2020-12-21 23:36:59 -0500

    added route for modifying projects

--- backend/api/core/urls.py
+++ backend/api/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_all_todos, get_all_projects, delete_todo
+from .views import get_all_todos, get_all_projects, delete_todo, modify_projects
 
 urlpatterns = [
     path("", get_all_projects),
+    path("projects/modify/<int:pk>", modify_projects),
     path("todos-all", get_all_todos),
     path("delete/todo/<int:pk>", delete_todo)
 ]
