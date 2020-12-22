03b35778779d7ec782e3c6bae6643b5545d7514d
blackpandan <faithgstc54@gmail.com>
2020-12-22 01:12:41 -0500

    renamed routes

--- backend/api/core/urls.py
+++ backend/api/core/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import get_all_todos, get_all_projects, modify_todo, modify_projects
 
 urlpatterns = [
-    path("", get_all_projects),
+    path("projects/all", get_all_projects),
     path("projects/modify/<int:id>", modify_projects),
-    path("todos-all", get_all_todos),
-    path("delete/todo/<int:pk>", modify_todo)
+    path("todos/all", get_all_todos),
+    path("todos/modify/<int:pk>", modify_todo)
 ]
