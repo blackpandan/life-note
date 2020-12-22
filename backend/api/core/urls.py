a03150da463f4975f4cc49c97412f8caa67d7e96
blackpandan <faithgstc54@gmail.com>
2020-12-22 01:09:18 -0500

    changed delete todo to modify todo

--- backend/api/core/urls.py
+++ backend/api/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import get_all_todos, get_all_projects, delete_todo, modify_projects
+from .views import get_all_todos, get_all_projects, modify_todo, modify_projects
 
 urlpatterns = [
     path("", get_all_projects),
     path("projects/modify/<int:id>", modify_projects),
     path("todos-all", get_all_todos),
-    path("delete/todo/<int:pk>", delete_todo)
+    path("delete/todo/<int:pk>", modify_todo)
 ]
