30ee01aefc1f7b606525e4926c79493c570af4a0
blackpandan <faithgstc54@gmail.com>
2020-12-23 05:41:40 -0500

    added obtain token route

--- backend/api/core/urls.py
+++ backend/api/core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import get_all_todos, get_all_projects, modify_todo, modify_projects
 
+#for rest_framework imports
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path("projects/all", get_all_projects),
+    path("auth/token", obtain_auth_token),
     path("projects/modify/<int:id>", modify_projects),
     path("todos/all", get_all_todos),
     path("todos/modify/<int:pk>", modify_todo)
