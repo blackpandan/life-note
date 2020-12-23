597a14e93a55ac5ed33dddfe1e1f1fdea025b01b
blackpandan <faithgstc54@gmail.com>
2020-12-23 03:17:54 -0500

    added automatic token creation function in models.py

--- backend/api/core/models.py
+++ backend/api/core/models.py
@@ -1,5 +1,6 @@
 # for django imports
 from django.db import models
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -20,3 +21,7 @@ class Project(models.Model):
 
 
 # this is for the automatic token creation
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
