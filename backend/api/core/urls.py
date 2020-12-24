5867a16139fc6ea1dd09992cbb440d2dd039d605
blackpandan <faithgstc54@gmail.com>
2020-12-23 20:37:36 -0500

    added route for registering user

--- backend/api/core/urls.py
+++ backend/api/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import get_all_todos, get_all_projects, modify_todo, modify_projects, get_user_details
+from .views import get_all_todos, get_all_projects, modify_todo, modify_projects, get_user_details, register_user
 
 #for rest_framework imports
 from rest_framework.authtoken.views import obtain_auth_token
@@ -10,5 +10,6 @@ urlpatterns = [
     path("projects/modify/<int:id>", modify_projects),
     path("todos/all", get_all_todos),
     path("todos/modify/<int:pk>", modify_todo),
-    path("user/details", get_user_details)
+    path("user/auth/details", get_user_details),
+    path("user/auth/register", register_user)
 ]
