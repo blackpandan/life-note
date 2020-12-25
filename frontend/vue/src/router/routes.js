3cd5a810f58866abbde873282aed944f93a29391
blackpandan <faithgstc54@gmail.com>
2020-12-25 10:38:50 -0500

    edited routes.js, changed the landing route

--- frontend/vue/src/router/routes.js
+++ frontend/vue/src/router/routes.js
@@ -27,6 +27,11 @@ const routes = [
     component: Login
   },
   {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+  {
     path: "/register",
     name: "register",
     component: Register
