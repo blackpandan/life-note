76c8bb83d69dc0082d11f3a0ee4523ee38fd49b7
blackpandan <faithgstc54@gmail.com>
2020-12-25 04:25:24 -0500

    added OneToOne Field of User to all models

--- backend/api/core/models.py
+++ backend/api/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 # for rest_framework imports
 from rest_framework.authtoken.models import Token
@@ -13,11 +14,13 @@ class Todo(models.Model):
     body=models.CharField(max_length=225)
     done=models.BooleanField(default=False)
     pause=models.BooleanField(default=False)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, default=None)
 
 class Project(models.Model):
     title=models.CharField(max_length=255)
     body=models.TextField()
     time = models.DateTimeField(auto_now_add=True)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, default=None)
 
 
 # this is for the automatic token creation
