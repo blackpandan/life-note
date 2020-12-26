8b60c615ae2c158ad5c00a472e1e5a4d2eaa53ee
blackpandan <faithgstc54@gmail.com>
2020-12-26 07:31:15 -0500

    changed initila route

--- frontend/vue/src/router/routes.js
+++ frontend/vue/src/router/routes.js
@@ -9,7 +9,7 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/home',
+    path: '/',
     name: 'Home',
     component: Home
   },
@@ -20,11 +20,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: "/",
-    name: "login",
-    component: Login
   },
   {
     path: "/login",
