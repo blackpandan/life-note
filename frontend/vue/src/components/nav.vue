7a33a012f692a2cab6598486119e0421bc196edf
blackpandan <faithgstc54@gmail.com>
2020-12-30 16:14:18 -0500

    edited nav, reactivity for icons refused to work

--- frontend/vue/src/components/nav.vue
+++ frontend/vue/src/components/nav.vue
@@ -15,26 +15,34 @@
       <!-- For Tablets and Other Bigger Screens-->
       <v-toolbar-items class="d-none d-sm-flex">
 
-        <v-btn dark color="purple accent-3" depressed to="/">
+        <v-btn dark color="purple accent-3" depressed to="/" v-if="isAuthenticated">
           <v-icon left>home</v-icon>
           <span class="font-weight-regular homeText">home</span>
         </v-btn>
         
-        <v-btn dark color="purple accent-3" depressed to="/login">
+        <v-btn dark color="purple accent-3" depressed to="/login" v-if="!isAuthenticated">
           <v-icon left>person</v-icon>
           <span class="font-weight-regular loginText">login</span>
         </v-btn>
       
+        <v-btn dark color="purple accent-3" depressed to="/logout" v-if="isAuthenticated">
+          <v-icon left>logout</v-icon>
+          <span class="font-weight-regular loginText">logout</span>
+        </v-btn>
+
       </v-toolbar-items>
       
       <!-- For Mobile Phones And Screen Smaller Than 600px -->
       <v-toolbar-items class="d-flex d-sm-none">
         
-        <v-btn icon to="/">
+        <v-btn icon to="/" v-if="isAuthenticated">
           <v-icon>home</v-icon>
         </v-btn>
-        <v-btn icon to="/login">
+        <v-btn icon to="/login" v-if="!isAuthenticated">
           <v-icon>person</v-icon>
+        </v-btn>
+         <v-btn icon to="/logout" v-if="isAuthenticated">
+          <v-icon>logout</v-icon>
         </v-btn>
       
       </v-toolbar-items>
@@ -53,7 +61,8 @@
         
         <v-card flat class="grey lighten-4">
           <v-card-title class="ma-0 pa-2">anonymous</v-card-title>
-          <v-card-subtitle class="pa-0 pa-2">login@gmail.com</v-card-subtitle>
+          <v-card-subtitle class="pa-0 pa-2" v-if="!isAuthenticated">login@gmail.com</v-card-subtitle>
+          <!-- <v-card-subtitle class="pa-0 pa-2" v-else>{{user.email}}</v-card-subtitle> -->
         </v-card>
         <v-divider color="font-size-5 purple accent-3"></v-divider>
       
@@ -63,7 +72,7 @@
       <v-list rounded>
         <v-list-item-group>
           
-          <v-list-item to="/">
+          <v-list-item to="/" v-if="isAuthenticated">
             <v-list-item-icon>
               <v-icon>home</v-icon>
             </v-list-item-icon>
@@ -72,7 +81,7 @@
             </v-list-item-content>
           </v-list-item>
 
-          <v-list-item to="/projects">
+          <v-list-item to="/projects" v-if="isAuthenticated">
             <v-list-item-icon>
               <v-icon>assignment</v-icon>
             </v-list-item-icon>
@@ -80,13 +89,21 @@
               <v-list-item-title>Projects</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
-
-          <v-list-item to="/login">
+          
+          <v-list-item to="/login" v-if="!isAuthenticated">
             <v-list-item-icon>
               <v-icon>login</v-icon>
             </v-list-item-icon>
             <v-list-item-content>
               <v-list-item-title>login</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item to="/logout" v-if="isAuthenticated">
+            <v-list-item-icon>
+              <v-icon>logout</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>logout</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         
@@ -102,8 +119,21 @@ export default {
     return {
       // This controls the drawer visibility
       drawer: false,
+      isAuthenticated: false
     };
   },
+  // created() {
+  //   let token = localStorage.getItem("isAuthenticated_lifenote");
+  //   if (token != null || token != undefined){
+  //     this.isAuthenticated = true;
+  //   }
+  // },
+  mounted() {
+    let token = localStorage.getItem("isAuthenticated_lifenote");
+    if (token != null || token != undefined){
+      this.isAuthenticated = true;
+    }
+  }
 };
 </script>
 
