251d3e0be0445934ab1591b99f22bec39bdb1655
blackpandan <faithgstc54@gmail.com>
2020-12-30 16:13:24 -0500

    created Logout view

---
+++ frontend/vue/src/views/Logout.vue
@@ -0,0 +1,18 @@
+<template>
+<p>logout</p>
+</template>
+
+<script>
+export default{
+beforeMount(){
+localStorage.removeItem("isAuthenticated_lifenote");
+localStorage.removeItem("token_lifenote");
+// this.$forceUpdate();
+this.$router.push("/")
+}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
