9c98aa7fa22154aa6d09c37cbd39c951794b0f65
blackpandan <faithgstc54@gmail.com>
2020-12-31 10:24:58 -0500

    edited logout view

--- frontend/vue/src/views/Logout.vue
+++ frontend/vue/src/views/Logout.vue
@@ -1,15 +1,34 @@
 <template>
-<p>logout</p>
+    <div>
+        <p>Are you sure you want to log out</p>
+        <v-btn small depressed color="success" v-on:click="logout()">
+            <span>yes</span>
+        </v-btn>
+        <v-btn small depressed color="error" class="ml-3" v-on:click="notLogout()">
+            <span>no</span>
+        </v-btn>
+    </div>
 </template>
 
 <script>
 export default{
-beforeMount(){
-this.$emit("recant");
-localStorage.removeItem("isAuthenticated_lifenote");
-localStorage.removeItem("token_lifenote");
-// this.$forceUpdate();
+
+methods: {
+logout(){
+    localStorage.removeItem("token_lifenote");
+    localStorage.removeItem("isAuthenticated_lifenote");
+    this.$emit("recant")
+    this.$router.push("/login")
+},
+notLogout(){
 this.$router.push("/")
+}
+},
+beforeMount(){
+    let token = localStorage.getItem("token_lifenote");
+    if (token == null || token == undefined){
+        this.$router.push("/login")
+    }
 }
 }
 </script>
