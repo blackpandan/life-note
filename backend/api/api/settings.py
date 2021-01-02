de8c80967a79e83d392d04ba7b1a803c19d9bdb1
blackpandan <faithgstc54@gmail.com>
2021-01-02 09:05:33 -0500

    edited backend

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -137,3 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfile'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
